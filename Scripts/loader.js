d0ab54b2b6066104231036f9feb75d8c9f1c9533
Andres <csi_union@yahoo.com>
2013-06-14 13:06:22 -0600

    Initialize commit

---
+++ Scripts/loader.js
@@ -0,0 +1,20 @@
+﻿var jewel = {};
+// wait until main doc is loaded
+window.addEventListener("load", function () {
+    // start dynamic loading
+    Modernizr.load([
+        {
+            // always loaded
+            load: [
+                "scripts/sizzle.js",
+                "scripts/dom.js",
+                "scripts/game.js"
+            ],
+            // waiting until main document is loaded
+            complete: function () {
+                // console.log("All files loaded!");
+                alert("Success");
+            }
+        }
+    ]);
+}, false);
